b823bfd6d72d37179ad287bf3dc87cc75da2e61d
LazzyBird <113029379+LazzyBird@users.noreply.github.com>
2023-04-16 20:08:40 +0300

    Update api connection
     from codesandbox working version

--- src/script.js
+++ src/script.js
@@ -9,7 +9,7 @@ let days = [
   "Wednesday",
   "Thursday",
   "Friday",
-  "Saturday",
+  "Saturday"
 ];
 let day = days[now.getDay()];
 let months = [
@@ -24,62 +24,14 @@ let months = [
   "September",
   "October",
   "November",
-  "December",
+  "December"
 ];
 let month = months[now.getMonth()];
 let hours = now.getHours();
 let minutes = now.getMinutes();
 //let seconds = now.getSeconds();  - how to do continuous refreshing withiut page update
+// fix case when minutes is shown as 21:1
 actualDate.innerHTML = `${day}, ${date} of ${month}, ${year}, ${hours}:${minutes}`;
-function formatDate() {
-  return `${day}, ${month} ${date}, ${year}, ${hours}, ${minutes}, ${seconds}`;
-}
-function locateCity(event) {
-  let city = document.querySelector("#city-header");
-  let cityInput = document.querySelector("#cityInput");
-  let cityName = cityInput.value.trim();
-  if (cityName === "") {
-    cityInput.placeholder = "Please enter a city";
-    cityInput.value = "";
-    return;
-  }
-
-  city.innerHTML = cityName;
-  cityInput.value = "";
-}
-
-document.getElementById("search-city").addEventListener("click", locateCity);
-document
-  .getElementById("cityInput")
-  .addEventListener("keydown", function (event) {
-    if (event.key === "Enter") {
-      event.preventDefault();
-      locateCity();
-    }
-  });
-let isCelsius = true; // Set default unit to Celsius
-const currentTemp = document.querySelector("#currentTemp");
-const unit = document.querySelector("#unit");
-
-function convertTemp() {
-  let temp = parseFloat(currentTemp.textContent);
-
-  if (isCelsius) {
-    // Convert Celsius to Fahrenheit
-    temp = temp * 1.8 + 32;
-    isCelsius = false;
-    unit.textContent = "ºF";
-  } else {
-    // Convert Fahrenheit to Celsius
-    temp = (temp - 32) / 1.8;
-    isCelsius = true;
-    unit.textContent = "ºC";
-  }
-
-  currentTemp.textContent = temp.toFixed(1);
-}
-
-document.querySelector("#convertBtn").addEventListener("click", convertTemp);
 
 let apiKey = "4085d4e1d22f7753a9278110dff3ae74";
 let cityName;
